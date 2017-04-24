87c723cdd2f8168deca6acf97986dbf9d4d46348
codrinleonte <codryn_14@yahoo.com>
2017-04-25 02:59:05 +0300

    Pagina noua+schimbari in folderul resurse
    am adaugat o pagina Home pentru utilizatorul de tip copil al aplicatiei si am adaugat cateva fisiere in folderul de resurse

--- TWProject/TWProject/frontEnd/javascript/javascript.js
+++ TWProject/TWProject/frontEnd/javascript/javascript.js
@@ -1,4 +1,4 @@
-
+var questionCount = 0;
 
 function showImageMed() {
   document.getElementById('imageMed').style.content = "url('../images/Sonic_sonicx.png')";
@@ -7,7 +7,7 @@ function showImageMed() {
    
 }
 function showImageEasy() {
-   document.getElementById('imageMed').style.content = "url('../images/yin.png')";
+   document.getElementById('imageMed').style.content = "url('../images/yin_by_lucius4277-d53xsf1.png')";
    document.getElementById('imageMed').style.display = "block";
    document.getElementById('StartBut').style.display = "block";
 }
@@ -20,4 +20,30 @@ function showImageHard() {
 
 function showDifficultyForm(){
 	document.getElementById('difficultyForm').style.display = "block";
+}
+
+
+function nextQuestion(){
+	
+	if(questionCount===7){
+		document.getElementById('finishButton').style.display = "block";
+		document.getElementById('nextButton').style.display = "none";
+		exit;
+	}
+	
+	
+	questionCount++;
+	
+	
+	document.getElementById('quest').innerHTML = "Intrebare".concat(questionCount.toString());;
+	var r = document.getElementsByTagName("label")   
+
+
+	
+	r[0].innerHTML ="answerA".concat(questionCount.toString());
+	r[1].innerHTML ="answerB".concat(questionCount.toString());
+	r[2].innerHTML ="AnswerC".concat(questionCount.toString());
+	r[3].innerHTML ="answerD".concat(questionCount.toString());
+    
+	
 }
