b64bcdfbd0a2dccdd6a23d397ba7947d1abd79cf
RaresDima <rares.dima96@yahoo.com>
2017-05-02 23:49:32 +0300

    Initial Project setup
    Signed-off-by: RaresDima <rares.dima96@yahoo.com>

--- ProiectIPB7/back-end/elements/projects/Project.java
+++ ProiectIPB7/back-end/elements/projects/Project.java
@@ -1,12 +1,48 @@
 package elements.projects;
 
+import elements.users.types.Student;
+
 public class Project {
 
     private String title;
+    private Student student;
+
+    // TODO Add any other necessary data members
 
 
-    Project(String title) {
+    public Project() {
+
+    }
+    public Project(String title, Student student) {
+        this.title = title;
+        this.student = student;
+    }
+
+    // TODO Add any other necessary constructors
+
+
+
+
+
+    // TODO Add any other necessary metods
+
+
+
+
+
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
         this.title = title;
     }
 
+    public Student getStudent() {
+        return student;
+    }
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    // TODO Add any other necessary getters/setters
 }
