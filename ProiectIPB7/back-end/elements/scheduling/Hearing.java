f7f70f5820d0339e8b98979c701dc7fbae558c81
RaresDima <rares.dima96@yahoo.com>
2017-05-03 00:09:39 +0300

    Initial Hearing setup
    Signed-off-by: RaresDima <rares.dima96@yahoo.com>

--- ProiectIPB7/back-end/elements/scheduling/Hearing.java
+++ ProiectIPB7/back-end/elements/scheduling/Hearing.java
@@ -1,4 +1,40 @@
 package elements.scheduling;
 
+import java.sql.Timestamp;
+
 public class Hearing {
+
+    private Timestamp time;
+
+    // TODO Add any other necessary data members(might be complete, need to consult)
+
+
+
+
+
+    public Hearing() {}
+    public Hearing(Timestamp time) {
+        this.time = time;
+    }
+
+    // TODO Add any other necessary constructors(might be complete, need to consult)
+
+
+
+
+
+    // TODO Add any other necessary methods(might be complete, need to consult)
+
+
+
+
+
+    public Timestamp getTime() {
+        return time;
+    }
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+
+    // TODO Add any other necessary data members(might be complete, need to consult)
 }
