23c37eeeb701a09a47de9c5679f5cdfcf11f1686
Rusu Cristian <ruscris2@gmail.com>
2017-05-05 17:28:30 +0300

    Meniu debug componenta home + tipuri cont

--- ProiectIPB7/frontend/src/app/dashboard/home/home.component.ts
+++ ProiectIPB7/frontend/src/app/dashboard/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
 
-  constructor() { }
+    userClass = 1;
 
-  ngOnInit() {
-  }
+    onStudentCPClick(){
+        this.userClass = 1;
+    }
 
+    onProfesorCPClick(){
+        this.userClass = 2;
+    }
+
+    onSecretarCPClick(){
+        this.userClass = 3;
+    }
+
+    onAdminCPClick(){
+        this.userClass = 4;
+    }
 }
