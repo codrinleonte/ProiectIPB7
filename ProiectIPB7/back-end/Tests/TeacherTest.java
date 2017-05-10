2be29ab5d7a4cd03a1535ffd96879bbca5cf4623
Florescu Razvan <rikimoo@users.noreply.github.com>
2017-05-10 13:42:01 +0300

    Am adaugat fisierele de test

---
+++ ProiectIPB7/back-end/Tests/TeacherTest.java
@@ -0,0 +1,84 @@
+package types;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TeacherTest {
+
+	@Test
+	public void testTeacher() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTeacherVectorOfStudent() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNumberOfStudents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCompareTo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetCoordinatedStudents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetCoordinatedStudents() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRights() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetRights() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetUsername() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEmail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetEmail() {
+		fail("Not yet implemented");
+	}
+
+}
