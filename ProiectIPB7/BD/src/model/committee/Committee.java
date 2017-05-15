2b515629ae24052707f9843624396e35d9144740
OttoSoft <ottosoftwares@gmail.com>
2017-05-15 14:43:54 +0300

    Implementare BD
    Schelet clase model modificate, scripturi de setare a bazei de date si de umplere tabele, implementarea login-ului,inregistrarii( fara trimiterii de email ) , verificare cont

--- ProiectIPB7/BD/src/model/committee/Committee.java
+++ ProiectIPB7/BD/src/model/committee/Committee.java
@@ -1,7 +1,6 @@
 package model.committee;
 
 import java.sql.Timestamp;
-import java.util.Date;
 import java.util.Vector;
 import model.users.types.Secretary;
 import model.users.types.Teacher;
@@ -14,6 +13,55 @@ public class Committee {
 	private Timestamp       start;
 	private Timestamp       end;
 	private String          tip;
+	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public Secretary getSecretary() {
+		return secretary;
+	}
+	
+	public void setSecretary(Secretary secretary) {
+		this.secretary = secretary;
+	}
+	
+	public Vector<Teacher> getTeachers() {
+		return teachers;
+	}
+	
+	public void setTeachers(Vector<Teacher> teachers) {
+		this.teachers = teachers;
+	}
+	
+	public Timestamp getStart() {
+		return start;
+	}
+	
+	public void setStart(Timestamp start) {
+		this.start = start;
+	}
+	
+	public Timestamp getEnd() {
+		return end;
+	}
+	
+	public void setEnd(Timestamp end) {
+		this.end = end;
+	}
+	
+	public String getTip() {
+		return tip;
+	}
+	
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
 	
 	
 
--- ProiectIPB7/BD/src/model/projects/Projects.java
+++ ProiectIPB7/BD/src/model/projects/Projects.java
@@ -16,4 +16,69 @@ public class Projects {
 	private Timestamp time;
 	private Mark      mark; 
 
+	public void setId ( int id ){
+		this.id = id;
+	}
+	
+	public int getId(){
+		return id;
+	}
+	
+	public void setTitle( String titlu ){
+		this.title = titlu;
+	}
+	
+	public String getTilte(){
+		return title;
+	}
+	
+	public void setDocumentLinks ( Vector<String> documentLinks ){
+		this.documentLinks=documentLinks;
+	}
+	
+    public Vector<String> getDocumentLinks(){
+	   return documentLinks;
+    }
+   
+    public void setTime( Timestamp time ){
+    	this.time=time;
+    }
+    
+    public Timestamp getTime(){
+    	return time;
+    }
+    
+    public void setMark( Mark mark ){
+    	this.mark=mark;
+    }
+    
+    public Mark getMark(){
+    	return mark;
+    }
+
+	public Committee getCommittee() {
+		return committee;
+	}
+
+	public void setCommittee(Committee committee) {
+		this.committee = committee;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	public String getTip() {
+		return tip;
+	}
+
+	public void setTip(String tip) {
+		this.tip = tip;
+	}
+	
+	
 }
