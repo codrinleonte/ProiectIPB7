4de0e44f97d9eea08ec86668f74423ee7317f243
Rusu Cristian <ruscris2@gmail.com>
2017-05-23 22:50:21 +0300

    Home button glow animation

--- ProiectIPB7/frontend/src/app/dashboard/home/panel-button/panel-button.component.ts
+++ ProiectIPB7/frontend/src/app/dashboard/home/panel-button/panel-button.component.ts
@@ -1,11 +1,30 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from "@angular/animations";
 
 @Component({
-  selector: 'app-panel-button',
-  templateUrl: './panel-button.component.html',
-  styleUrls: ['./panel-button.component.css']
+    selector: 'app-panel-button',
+    templateUrl: './panel-button.component.html',
+    styleUrls: ['./panel-button.component.css'],
+    animations: [
+        trigger('glowAnim', [
+            state('hidden', style({ 'box-shadow': 'none' })),
+            state('visible', style({ 'box-shadow': '0px 0px 12px #0054ff'})),
+            transition('hidden => visible', animate('300ms ease-in')),
+            transition('visible => hidden', animate('300ms ease-out'))
+        ])
+    ]
 })
 export class PanelButtonComponent {
     @Input() icon: string;
     @Input() desc: string;
+
+    animState: string = 'hidden';
+
+    onHover(){
+        this.animState = 'visible';
+    }
+
+    onHoverEnd(){
+        this.animState = 'hidden';
+    }
 }
